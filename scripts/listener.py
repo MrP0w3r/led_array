64315752ea429aa74e9252d3b132721268782eb2
MrP0w3r <mjpow3@gmail.com>
2019-07-17 16:10:01 +1000

    added other files

---
+++ scripts/listener.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber('chatter', String, callback)
+
+    rospy.spin()    # spin() simply keeps python from exiting until this node is stopped
+
+if __name__ == '__main__':
+    listener()
